7dc1a042523d2c966dd665e4dc0c33b2277e8ebf
andigandhi <andigrasser13@gmail.com>
2022-02-04 17:29:02 +0100

    bisq

--- telegramVar.php
+++ telegramVar.php
@@ -1,6 +1,6 @@
 <?php
     $telegrambot = "5219388360:AAGCU_-swrUtTT9GmfQARaY2r1UpnyJgF2Q";
-    $telegramchatid = -1001701410334;
+    $telegramchatid = -100698532846;
 
     $path = "https://api.telegram.org/bot" . $telegrambot;
 ?>
